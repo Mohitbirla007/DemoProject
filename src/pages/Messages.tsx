7124c68796bb9128d0d5217dda1a529fddfed185
ritesh-lucaplus <87077931+ritesh-lucaplus@users.noreply.github.com>
2021-10-11 20:11:59 +0530

    add eventwill launch function

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,46 +1,84 @@
-import React, {useState} from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonList, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent, IonCard, IonAvatar, IonNote, IonButtons, IonButton, IonIcon } from '@ionic/react';
-import { addOutline, pencil } from 'ionicons/icons';
-import './Messages.css';
+import React, { useState } from "react";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonToggle,
+  IonRadio,
+  IonCheckbox,
+  IonItemSliding,
+  IonItemOption,
+  IonItemOptions,
+  IonContent,
+  IonCard,
+  IonAvatar,
+  IonNote,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  useIonViewDidEnter,
+} from "@ionic/react";
+import { addOutline, pencil } from "ionicons/icons";
+import "./Messages.css";
 
 const Messages: React.FC = () => {
-    const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+
+  useIonViewDidEnter(() => {
+    console.log("ionViewDidEnter event fired");
+  });
+
   return (
-  <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle className="title" >Messages</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-        <IonSearchbar className="searchbar" value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle className="title">Messages</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonSearchbar
+          className="searchbar"
+          value={searchText}
+          onIonChange={(e) => setSearchText(e.detail.value!)}
+        ></IonSearchbar>
 
-      {/*-- List of Sliding Items --*/}
-      <IonList>
-
-      <h6 className="suggested">Messages</h6>
-    <IonCard className="director" >
-        <IonItemSliding>
-          <IonItem lines="none" >
-          <IonAvatar slot="start">
-            <img src="./avatar-finn.png" />
-          </IonAvatar>
-            <IonLabel>
-            <h2>Finn</h2>
-            <p>Listen, I've had a pretty messed up day...</p>
-            </IonLabel>
-            <IonNote slot="end">
-              10:45 AM
-            </IonNote>
-          </IonItem>
-          <IonItemOptions side="end">
-            <IonItemOption color="danger" onClick={() => {}}>Mute</IonItemOption>
-                        <IonItemOption className="messagedelete" color="warning" onClick={() => {}}>Delete</IonItemOption>
-          </IonItemOptions>
-        </IonItemSliding>
-     </IonCard>
-      </IonList>
-    </IonContent>
+        {/*-- List of Sliding Items --*/}
+        <IonList>
+          <h6 className="suggested">Messages</h6>
+          <IonCard className="director">
+            <IonItemSliding>
+              <IonItem lines="none">
+                <IonAvatar slot="start">
+                  <img src="./avatar-finn.png" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Finn</h2>
+                  <p>Listen, I've had a pretty messed up day...</p>
+                </IonLabel>
+                <IonNote slot="end">10:45 AM</IonNote>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption color="danger" onClick={() => {}}>
+                  Mute
+                </IonItemOption>
+                <IonItemOption
+                  className="messagedelete"
+                  color="warning"
+                  onClick={() => {}}
+                >
+                  Delete
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          </IonCard>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
