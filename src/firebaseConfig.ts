f834ab7c4d5538f4541799d73eea640bed53a417
ritesh-lucaplus <87077931+ritesh-lucaplus@users.noreply.github.com>
2021-10-11 19:24:45 +0530

    setup firebase for auth login

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,17 +1,17 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 /* import 'firebase/auth'; */
-import { toast } from './toast' 
+import { toast } from "./toast";
 
 const config = {
-    apiKey: "AIzaSyDNtqzEYjyz90Pnj6cfTaANa_uLx0pH8PE",
-    authDomain: "indi-da614.firebaseapp.com",
-    databaseURL: "https://indi-da614-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "indi-da614",
-    storageBucket: "indi-da614.appspot.com",
-    messagingSenderId: "662956774567",
-    appId: "1:662956774567:web:35cdae712abce7e9cef782",
-    measurementId: "G-J6W8CMV4BH"
-}
+  apiKey: "AIzaSyAsgCXMa0SMNIG5hL2GsS4xpl_Jj-q3IjU", //updated
+  authDomain: "indi-d4f1b.firebaseapp.com", //updated
+  databaseURL: "https://indi-d4f1b-default-rtdb.firebaseio.com/", //updated
+  projectId: "indi-d4f1b", //updated
+  storageBucket: "indi-da614.appspot.com",
+  messagingSenderId: "356472484887", //updated
+  //   appId: "1:662956774567:web:35cdae712abce7e9cef782",
+  //   measurementId: "G-J6W8CMV4BH",
+};
 
 const firebaseApp = firebase.initializeApp(config);
 const db = firebaseApp.firestore();
@@ -23,34 +23,32 @@ const providerFacebook = new firebase.auth.FacebookAuthProvider();
 export { auth, storageRef, providerFacebook, providerGoogle };
 export default db;
 
-
-
-
-
-
-
 export async function loginUser(username: string, password: string) {
-    const email = `${username}`
-    try {
-    const res = await firebase.auth().signInWithEmailAndPassword(email, password)
+  const email = `${username}`;
+  try {
+    const res = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
 
-    console.log(res)
-    return true
-    } catch (error) {
-        console.log(error)
-        return false
-    }
+    console.log(res);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-export async function registerUser (username: string, password: string) {
-    const email = `${username}`
-    try {
-        const res = await firebase.auth().createUserWithEmailAndPassword(email, password)
-        console.log(res)
-        return true
-    } catch(error) {
-        console.log(error)
-        toast(error.message, 4000)
-        return false
-    }
+export async function registerUser(username: string, password: string) {
+  const email = `${username}`;
+  try {
+    const res = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    console.log(res);
+    return true;
+  } catch (error) {
+    console.log(error);
+    toast(error.message, 4000);
+    return false;
+  }
 }
